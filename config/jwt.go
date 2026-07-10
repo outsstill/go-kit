abb8cae48ac16567d0d525169247a8313e8e4de5
zero <zhangzhuang@dongyou.com>
2026-07-10 17:01:20 +0800

    up

--- config/jwt.go
+++ config/jwt.go
@@ -3,12 +3,12 @@ package config
 import "github.com/outsstill/go-kit/jwt"
 
 type JWTConfig struct {
-	Name       string `mapstructure:"name" json:"name"`
-	Key        string `mapstructure:"key" json:"key"`
-	MaxRefresh int64  `mapstructure:"max_refresh" json:"max_refresh"`
-	Timezone   string `mapstructure:"timezone" json:"timezone"`
-	Expires    int64  `mapstructure:"expires" json:"expires"`
-	Type       int32  `mapstructure:"type" json:"type"`
+	Name       string       `mapstructure:"name" json:"name"`
+	Key        string       `mapstructure:"key" json:"key"`
+	MaxRefresh int64        `mapstructure:"max_refresh" json:"max_refresh"`
+	Timezone   string       `mapstructure:"timezone" json:"timezone"`
+	Expires    int64        `mapstructure:"expires" json:"expires"`
+	Type       jwt.JWT_TYPE `mapstructure:"type" json:"type"`
 }
 
 func (c JWTConfig) ToJWT() jwt.Config {
