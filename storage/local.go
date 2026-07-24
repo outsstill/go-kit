736741e71aea1b9d4d9d4ddfbe6b8fa543e45753
zero <zhangzhuang@dongyou.com>
2026-07-24 11:28:56 +0800

    up

--- storage/local.go
+++ storage/local.go
@@ -55,6 +55,7 @@ func (l *LocalStorage) Put(ctx context.Context, in *UploadRequest) (*FileObj, er
 	f.Size = size
 	f.ID = uuid.NewString()
 	f.ContentType = in.ContentType
+	f.MimeType = in.MimeType
 	f.Path = strings.Trim(realPath, "/")
 	f.Key = fullPath
 	f.Ext = GetFileExt(in.Filename)
